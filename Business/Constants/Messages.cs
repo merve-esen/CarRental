c3b9fbb1b1940e4594a373eb4ef7e22b4c2beeec
Merve Esen <merveesen834@gmail.com>
2022-02-08 12:19:00 +0300

    Update Messages.cs

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -6,11 +6,28 @@ namespace Business.Constants
 {
     public static class Messages
     {
+        // Brand
+        public static string BrandAdded = "Marka eklendi";
+        public static string BrandUpdated = "Marka güncellendi";
+        public static string BrandDeleted = "Marka silindi";
+        public static string BrandNameAlreadyExists = "Marka zaten mevcut";
+        public static string BrandNotFound = "Marka bulunamadı";
+
+        // Color
+        public static string ColorAdded = "Renk eklendi";
+        public static string ColorUpdated = "Renk güncellendi";
+        public static string ColorDeleted = "Renk silindi";
+        public static string ColorNameAlreadyExists = "Renk zaten mevcut";
+        public static string ColorNotFound = "Renk bulunamadı";
+
         // Car
-        public static string CarAdded = "Araba eklendi.";
-        public static string CarNameMinTwoCharacters = "Araba ismi minimum 2 karakter olmalıdır.";
-        public static string DailyPriceBiggerThanZero = "Fiyat 0'dan büyük olmalıdır.";
-        public static string ReturnDateIsNull = "Araba teslim edilmediği için kiralanamaz.";
+        public static string CarAdded = "Araba eklendi";
+        public static string CarUpdated = "Araba güncellendi";
+        public static string CarDeleted = "Araba silindi";
+        public static string CarNameMinTwoCharacters = "Araba ismi minimum 2 karakter olmalıdır";
+        public static string DailyPriceBiggerThanZero = "Fiyat 0'dan büyük olmalıdır";
+        public static string ReturnDateIsNull = "Araba teslim edilmediği için kiralanamaz";
+        public static string CarNotFound = "Araba bulunamadı";
 
         // Rental
         public static string CarRented = "Araba kiralandı";
@@ -19,9 +36,10 @@ namespace Business.Constants
 
         // CarImage
         public static string CarImageAdded = "Araba resmi eklendi";
+        public static string CarImageUpdated = "Araba resmi güncellendi";
+        public static string CarImageDeleted = "Araba resmi silindi";
         public static string CarImageLimitAchieved = "Bir araba için en fazla 5 resim yüklenebilir";
         public static string CarImageNotFound = "Araba resmi bulunamadı";
-        public static string CarImageUpdated = "Araba resmi güncellendi";
 
         // Authorization
         public static string AuthorizationDenied = "Yetkiniz yok";
