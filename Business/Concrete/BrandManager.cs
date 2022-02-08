95884ae1f9f1b6786b181f28754c35e7010b5b9d
Merve Esen <merveesen834@gmail.com>
2022-02-08 13:18:25 +0300

    Update BrandManager.cs

--- Business/Concrete/BrandManager.cs
+++ Business/Concrete/BrandManager.cs
@@ -1,11 +1,14 @@
 ﻿using Business.Abstract;
+using Business.Constants;
 using Business.ValidationRules.FluentValidation;
 using Core.Aspects.Autofac.Validation;
+using Core.Utilities.Business;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Business.Concrete
@@ -22,21 +25,39 @@ namespace Business.Concrete
         [ValidationAspect(typeof(BrandValidator))]
         public IResult Add(Brand brand)
         {
+            IResult result = BusinessRules.Run(CheckIfBrandNameExists(brand.Name));
+            if (result != null)
+            {
+                return result;
+            }
+
             _brandDal.Add(brand);
-            return new SuccessResult();
+            return new SuccessResult(Messages.BrandAdded);
         }
 
         [ValidationAspect(typeof(BrandValidator))]
         public IResult Update(Brand brand)
         {
+            IResult result = BusinessRules.Run(CheckIfBrandExists(brand.Id));
+            if (result != null)
+            {
+                return result;
+            }
+
             _brandDal.Update(brand);
-            return new SuccessResult();
+            return new SuccessResult(Messages.BrandUpdated);
         }
 
         public IResult Delete(Brand brand)
         {
+            IResult result = BusinessRules.Run(CheckIfBrandExists(brand.Id));
+            if (result != null)
+            {
+                return result;
+            }
+
             _brandDal.Delete(brand);
-            return new SuccessResult();
+            return new SuccessResult(Messages.BrandDeleted);
         }
 
         public IDataResult<List<Brand>> GetAll()
@@ -47,6 +68,36 @@ namespace Business.Concrete
         public IDataResult<Brand> GetById(int id)
         {
             return new SuccessDataResult<Brand>(_brandDal.Get(b => b.Id == id));
+        }
+
+        private IResult CheckIfBrandNameExists(string brandName)
+        {
+            if (string.IsNullOrEmpty(brandName))
+            {
+                return new ErrorResult(Messages.BrandNotFound);
+            }
+
+            var result = _brandDal.GetAll(b => b.Name.ToLower() == brandName.ToLower()).Any();
+            if (result)
+            {
+                return new ErrorResult(Messages.BrandNameAlreadyExists);
+            }
+            return new SuccessResult();
+        }
+
+        private IResult CheckIfBrandExists(int brandId)
+        {
+            if (brandId == 0)
+            {
+                return new ErrorResult(Messages.BrandNotFound);
+            }
+
+            var result = _brandDal.GetAll(b => b.Id == brandId).Any();
+            if (!result)
+            {
+                return new ErrorResult(Messages.BrandNotFound);
+            }
+            return new SuccessResult();
         }
     }
 }
