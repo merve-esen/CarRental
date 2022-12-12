8e80bff2311c969a7d6ad97eacca12fede9626a5
Merve Esen <merveesen834@gmail.com>
2022-12-12 15:32:06 +0300

    Update Messages.cs

--- Core/Constants/Messages.cs
+++ Core/Constants/Messages.cs
@@ -5,5 +5,7 @@
         public static string FileNotExist = "Dosya mevcut değil";
         public static string WrongFImageType= "Yanlış resim tipi";
         public static string WrongFileType = "Yanlış dosya tipi";
+
+        public static string WrongValidationType = "Bu bir doğrulama sınıfı değil";
     }
 }
